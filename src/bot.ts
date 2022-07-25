7d08a02a67899b8fa4e1699645a35bc556e8d649
WilliamAtUni <william.lam4@students.mq.edu.au>
2022-07-26 06:48:56 +1000

    do some code formatting

--- src/bot.ts
+++ src/bot.ts
@@ -1,13 +1,17 @@
-import { MessageEmbed, Intents } from 'discord.js';
-import { SlasherClient } from 'discord.js-slasher';
-import { excludeUser, sendExclusionMessage, surpassedRecordMessage } from './exclude';
-import { showHelpEmbed } from './embeds';
-import * as dotenv from 'dotenv';
+import { MessageEmbed, Intents } from "discord.js";
+import { SlasherClient } from "discord.js-slasher";
+import {
+  excludeUser,
+  sendExclusionMessage,
+  surpassedRecordMessage,
+} from "./exclude";
+import { showHelpEmbed } from "./embeds";
+import * as dotenv from "dotenv";
 dotenv.config();
-import fs from 'fs';
+import fs from "fs";
 
-let countingChannelId = '';
-let lastMessageAuthorId = '';
+let countingChannelId = "";
+let lastMessageAuthorId = "";
 let record = 1;
 let counter = 1;
 let usersOut = 0;
@@ -17,7 +21,7 @@ let data = {
   lastMessageAuthorId: lastMessageAuthorId,
   record: record,
   counter: counter,
-  usersOut: usersOut
+  usersOut: usersOut,
 };
 
 const client = new SlasherClient({
@@ -31,9 +35,9 @@ const client = new SlasherClient({
   token: process.env.TOKEN,
 });
 
-client.once('ready', async () => {
+client.once("ready", async () => {
   console.log(`${client.user.tag} is ready`);
-  data = loadData('data.json', data);
+  data = loadData("data.json", data);
 
   countingChannelId = data.countingChannelId;
   lastMessageAuthorId = data.lastMessageAuthorId;
@@ -43,15 +47,14 @@ client.once('ready', async () => {
   console.log(data);
 });
 
-client.on('command', async (context) => {
+client.on("command", async (context) => {
   // create a new thread
 
-  if (context.name === 'help') {
+  if (context.name === "help") {
     showHelpEmbed(context);
-  } else if (context.name === 'start') {
-
+  } else if (context.name === "start") {
     // if a channel already exists, delete it
-    if (countingChannelId !== '') {
+    if (countingChannelId !== "") {
       await context.server.guild.channels.delete(countingChannelId);
     }
 
@@ -59,13 +62,13 @@ client.on('command', async (context) => {
     // first, get the channel list
     let ChannelManager = context.server.guild.channels;
 
-    let countingChannel = await ChannelManager.create('Counting', {
+    let countingChannel = await ChannelManager.create("Counting", {
       rateLimitPerUser: 2,
       reason: `${context.user.username} used the start command to create a counting channel`,
     });
 
     countingChannelId = countingChannel.id;
-    console.log('Counting channel ID: ' + countingChannelId);
+    console.log("Counting channel ID: " + countingChannelId);
 
     await countingChannel.send(`
         :sparkles: Welcome to the **Counting Channel** :sparkles:\n
@@ -76,14 +79,19 @@ client.on('command', async (context) => {
 
     // save the channel id to the data file
     data.countingChannelId = countingChannelId;
-    fs.writeFileSync('data.json', JSON.stringify(data));
+    fs.writeFileSync("data.json", JSON.stringify(data));
 
-    await context.reply(new MessageEmbed().setTitle('Counting Channel created.'), true);
-  } else if (context.name === 'stop') {
+    await context.reply(
+      new MessageEmbed().setTitle("Counting Channel created."),
+      true
+    );
+  } else if (context.name === "stop") {
     // if there is no channel, do not do anything
-    if (countingChannelId === '') {
+    if (countingChannelId === "") {
       context.reply(
-        new MessageEmbed().setTitle('Unable to delete channel. No counting channel exists.'),
+        new MessageEmbed().setTitle(
+          "Unable to delete channel. No counting channel exists."
+        ),
         true
       );
       return;
@@ -95,13 +103,16 @@ client.on('command', async (context) => {
     await countingChannel.delete();
     data.usersOut = usersOut = 0;
 
-    context.reply(new MessageEmbed().setTitle('Counting channel has been deleted.'), true);
+    context.reply(
+      new MessageEmbed().setTitle("Counting channel has been deleted."),
+      true
+    );
 
     // save the channel id to the data file
-    countingChannelId = '';
+    countingChannelId = "";
     data.countingChannelId = countingChannelId;
-    fs.writeFileSync('data.json', JSON.stringify(data));
-  } else if (context.name === 'reset') {
+    fs.writeFileSync("data.json", JSON.stringify(data));
+  } else if (context.name === "reset") {
     // will do soon
     return;
     /* 
@@ -133,69 +144,78 @@ client.on('command', async (context) => {
         // get permission overrides
         let overrides = countingChannel.permissionOverwrites;
         */
-  } else if (context.name === 'clean') {
-    console.log('Cleaning channels...');
+  } else if (context.name === "clean") {
+    console.log("Cleaning channels...");
     // debugger;
     // clean the counting channels
     let ChannelManager = context.server.guild.channels;
     let countingChannels = ChannelManager.cache.filter(
-      (channel) => channel.name === 'counting',
+      (channel) => channel.name === "counting"
     );
 
     if (countingChannels.size === 0) {
       // no counting channels exist, send reply to user
-      context.reply(new MessageEmbed()
-              .setTitle('No counting channels exist.')
-              .setDescription('No channels have been removed.'), true);
+      context.reply(
+        new MessageEmbed()
+          .setTitle("No counting channels exist.")
+          .setDescription("No channels have been removed."),
+        true
+      );
       return;
     }
 
     for (let [countingChannelID] of countingChannels) {
-      console.log('Deleting channel: ' + countingChannelID);
+      console.log("Deleting channel: " + countingChannelID);
       await ChannelManager.delete(countingChannelID);
-      console.log('Channel deleted.');
+      console.log("Channel deleted.");
     }
 
-    console.log('Channels gone!\nRemoving data...');
+    console.log("Channels gone!\nRemoving data...");
     // remove the data
-    countingChannelId = '';
-    lastMessageAuthorId = '';
+    countingChannelId = "";
+    lastMessageAuthorId = "";
     data.countingChannelId = countingChannelId;
     data.lastMessageAuthorId = lastMessageAuthorId;
-    fs.writeFileSync('data.json', JSON.stringify(data));
+    fs.writeFileSync("data.json", JSON.stringify(data));
 
     // reply to the user
-    context.reply(new MessageEmbed().setTitle('Counting channels have been cleaned.'), true);
-  } else if (context.name === 'stats') {
+    context.reply(
+      new MessageEmbed().setTitle("Counting channels have been cleaned."),
+      true
+    );
+  } else if (context.name === "stats") {
     if (
-        context.channel.id !== countingChannelId ||
-        context.channel.type !== 'GUILD_TEXT'
-      )
-        return;
-    
+      context.channel.id !== countingChannelId ||
+      context.channel.type !== "GUILD_TEXT"
+    )
+      return;
+
     let channelCreationDate = context.channel.createdAt;
-    await context.reply(new MessageEmbed()
-            .setTitle('Counting channel stats')
-            .setDescription('Since the channel\'s creation at '+channelCreationDate+':')
-            .addFields(
-                { name: 'Users out', value: '`'+usersOut+'`'}
-            ), true);
+    await context.reply(
+      new MessageEmbed()
+        .setTitle("Counting channel stats")
+        .setDescription(
+          "Since the channel's creation at " + channelCreationDate + ":"
+        )
+        .addFields({ name: "Users out", value: "`" + usersOut + "`" }),
+      true
+    );
   }
 });
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
   // scan the message
   // initially, check if it's in the right channel
   if (
     message.channel.id !== countingChannelId ||
-    message.channel.type !== 'GUILD_TEXT'
+    message.channel.type !== "GUILD_TEXT"
   )
     return;
 
   // check if the last message is from the same user
   // if it is, ignore it
   if (message.author.id === lastMessageAuthorId) {
-    console.log('Ignoring message from same user.');
+    console.log("Ignoring message from same user.");
     return;
   }
 
@@ -203,8 +223,8 @@ client.on('messageCreate', async (message) => {
   data.lastMessageAuthorId = lastMessageAuthorId;
 
   // write the last message author id to the data file
-  console.log('Current number: ' + counter);
-  fs.writeFileSync('data.json', JSON.stringify(data));
+  console.log("Current number: " + counter);
+  fs.writeFileSync("data.json", JSON.stringify(data));
 
   // check if it's not the bot
   if (message.author.bot || message.author.id === client.user.id) return;
@@ -215,11 +235,10 @@ client.on('messageCreate', async (message) => {
   let numberRegex = /^\d+$/;
 
   // Just remove all non ascii chars
-  let sanitisedMessageContent = message.content.replace(/[^\x20-\x7F]/gu, ""); 
-
+  let sanitisedMessageContent = message.content.replace(/[^\x20-\x7F]/gu, "");
 
   if (!numberRegex.test(sanitisedMessageContent)) {
-      return; // it's not just a number
+    return; // it's not just a number
   }
 
   if (sanitisedMessageContent !== counter.toString()) {
@@ -231,12 +250,12 @@ client.on('messageCreate', async (message) => {
 
     // debugger;
 
-    await message.react('❌');
+    await message.react("❌");
 
     if (counter > record) {
       counter--;
       record = counter;
-      console.log('New record: ' + record);
+      console.log("New record: " + record);
       // send a message congratulating the user on surpassing the record
       await surpassedRecordMessage(message, user, record);
     }
@@ -246,26 +265,26 @@ client.on('messageCreate', async (message) => {
     await excludeUser(permissions, user);
 
     counter = 1;
-    console.log('The counter has been reset to ' + counter);
+    console.log("The counter has been reset to " + counter);
     data.record = record;
     data.counter = counter;
     data.usersOut++;
     usersOut++;
-    fs.writeFileSync('data.json', JSON.stringify(data));
+    fs.writeFileSync("data.json", JSON.stringify(data));
 
     return;
   }
 
   counter++;
-  console.log('Number incremented to ' + counter);
+  console.log("Number incremented to " + counter);
   data.counter = counter;
-  fs.writeFileSync('data.json', JSON.stringify(data));
+  fs.writeFileSync("data.json", JSON.stringify(data));
 });
 
 function loadData(file: string, init: object) {
   // if file exists, read and parse it
-  if(fs.existsSync(file)) {
-    return JSON.parse(fs.readFileSync(file, 'utf8'));
+  if (fs.existsSync(file)) {
+    return JSON.parse(fs.readFileSync(file, "utf8"));
   }
   // otherwise write the default data to file
   fs.writeFileSync(file, JSON.stringify(init));
--- src/embeds.ts
+++ src/embeds.ts
@@ -2,23 +2,24 @@ import { MessageEmbed } from "discord.js";
 import type { CommandContext } from "discord.js-slasher";
 
 export function showHelpEmbed(context: CommandContext) {
-    context.reply(new MessageEmbed()
-                    .setTitle('Help with roles!')
-                    .setDescription('A listing of commands that you can use')
-                    .setColor('#FFBCB5')
-                    .addFields(
-                        { name: '`/help`', value: 'Shows this message' },
-                        {
-                            name: '`/start`',
-                            value: 'Creates a new channel and sets the counter to 1',
-                        },
-                        { name: '`/stop`', value: 'Deletes the channel' },
-                        { 
-                            name: '`/stats`',
-                            value: 'Shows how many users have been excluded since channel creation'
-                        }
-        ),
-        true
-    );
+  context.reply(
+    new MessageEmbed()
+      .setTitle("Help with roles!")
+      .setDescription("A listing of commands that you can use")
+      .setColor("#FFBCB5")
+      .addFields(
+        { name: "`/help`", value: "Shows this message" },
+        {
+          name: "`/start`",
+          value: "Creates a new channel and sets the counter to 1",
+        },
+        { name: "`/stop`", value: "Deletes the channel" },
+        {
+          name: "`/stats`",
+          value:
+            "Shows how many users have been excluded since channel creation",
+        }
+      ),
+    true
+  );
 }
-
